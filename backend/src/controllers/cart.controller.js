06e90518f8c56609c5238bdd4bdb167738b16bf5
piyush72yaduvanshi <piyushyaduvanshi6574@gmail.com>
2026-02-13 17:57:41 +0530

    add cart controllers

---
+++ backend/src/controllers/cart.controller.js
@@ -0,0 +1,148 @@
+import mongoose from "mongoose";
+import { Cart } from "../models/cart.model";
+import { Product } from "../models/product.model";
+
+export const addToCart = async (req, res) => {
+  try {
+    const { userId, productId, quantity } = req.body;
+
+    if (!userId || !productId || quantity === undefined) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    if (
+      !mongoose.Types.ObjectId.isValid(userId) ||
+      !mongoose.Types.ObjectId.isValid(productId)
+    ) {
+      return res.status(400).json({ message: "Invalid userId and ProductId" });
+    }
+    if (quantity < 1) {
+      return res.status(400).json({ message: "Quantity must be at least 1" });
+    }
+    const products = await Product.findById(productId);
+    if (!products) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    const cart = await Cart.findOne({ userId });
+
+    if (!cart) {
+      cart = await Cart.create({
+        userId,
+        products: [{ productId, quantity, price: products.price }],
+      });
+
+      return res.status(200).json({
+        message: "Product Added to cart successfully",
+        cart,
+      });
+    }
+
+    const productIndex = cart.products.findIndex(
+      (item) => item.productId.toString() === productId,
+    );
+    if (productIndex > -1) {
+      cart.products[productIndex].quantity += quantity;
+    } else {
+      cart.products.push({
+        productId,
+        quantity,
+        price: products.price,
+      });
+    }
+    await cart.save();
+    res.status(200).json({
+      message: "Product Added to cart successfully",
+      cart,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error adding item to cart",
+      error: error.message,
+    });
+  }
+};
+export const updateCartItem = async (req, res) => {
+  try {
+    const { userId, productId, quantity } = req.body;
+
+    if (!userId || !productId || quantity === undefined) {
+      return res.status(400).json({ message: "All field are required" });
+    }
+    if (
+      !mongoose.Types.ObjectId.isValid(userId) ||
+      !mongoose.Types.ObjectId.isValid(productId)
+    ) {
+      return res.status(400).json({ message: "Invalid userId and ProductId" });
+    }
+    if (quantity < 1) {
+      return res.status(400).json({ message: "Quantity must be at least 1" });
+    }
+    const cart = await Cart.findById(userId);
+    if (!cart) {
+      return res.status(404).json({ message: "Cart not found" });
+    }
+    const product = cart.products.find(
+      (item) => item.productId.toString() === productId,
+    );
+    if (!product) {
+      return res.status(404).json({ message: "Product not found in cart" });
+    }
+
+    product.quantity = quantity;
+    await cart.save();
+    res.status(200).json({
+      message: "Cart item updated successfully",
+      cart,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error updating cart item",
+      error: error.message,
+    });
+  }
+};
+export const removeFromCart = async (req, res) => {
+  try {
+    const { userId, productId } = req.body;
+
+    if (!userId || !productId) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    if (
+      !mongoose.Types.ObjectId.isValid(userId) ||
+      !mongoose.Types.ObjectId.isValid(productId)
+    ) {
+      return res.status(400).json({ message: "Invalid userId and ProductId" });
+    }
+
+    const cart = await Cart.findById(userId);
+    if (!cart) {
+      return res.status(404).json({ message: "Cart not found" });
+    }
+    const initialLength = cart.products.length;
+    cart.products = cart.products.filter(
+      (item) => item.productId.toString() !== productId,
+    );
+    if (cart.products.length === initialLength) {
+      return res.status(404).json({ message: "Product not found in cart" });
+    }
+    await cart.save();
+    res.status(200).json({
+      message: "Product removed from the cart successfuly",
+      cart,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Error while removing item from cart",
+      error: error.message,
+    });
+  }
+};
+export const checkout = async (req, res) => {};
+export const getCartItems = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
---
+++ backend/src/controllers/order.controller.js
@@ -0,0 +1,15 @@
+import { Order } from "../models/order.model";
+
+export const createOrder = async (req, res) => {};
+export const acceptOrder = async (req, res) => {};
+export const rejectOrder = async (req, res) => {};
+export const assignDeliveryPerson = async (req, res) => {};
+export const updateOrderStatus = async (req, res) => {};
+export const cancelOrder = async (req, res) => {};
+
+export const getOrders = async (req, res) => {};
+export const getOrderById = async (req, res) => {};
+export const getOrdersByUserId = async (req, res) => {};
+export const getAllOrders = async (req, res) => {};
+
+
